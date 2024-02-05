8bd86e9fb9334ceb99981d55cce937b899ce9201
Saranyar04 <srajendran.laba@solvd.com>
2024-02-05 13:41:52 -0500

    API Tests

---
+++ src/main/java/com/solvd/carina/demo/api/user/GetUserMethods.java
@@ -0,0 +1,19 @@
+package com.solvd.carina.demo.api.user;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.config.Configuration;
+
+@Endpoint(url = "${api_url}/users", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "api/users/_get/rs.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class GetUserMethods extends AbstractApiMethodV2 {
+
+    public GetUserMethods() {
+        replaceUrlPlaceholder("api_url", Configuration.getRequired("api_url"));
+    }
+}
---
+++ src/test/java/com/solvd/carina/demo/APISampleTest.java
@@ -0,0 +1,125 @@
+package com.solvd.carina.demo;
+
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.solvd.carina.demo.api.post.*;
+import com.zebrunner.carina.api.apitools.validation.JsonCompareKeywords;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import com.zebrunner.carina.core.registrar.tag.Priority;
+import com.zebrunner.carina.core.registrar.tag.TestPriority;
+
+/**
+ * This sample shows how create REST API tests.
+ *
+ * @author qpsdemo
+ */
+public class APISampleTest implements IAbstractTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    /*@Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testCreateUser() throws Exception {
+        LOGGER.info("test");
+        setCases("4555,54545");
+        PostUserMethod postUserMethod = new PostUserMethod();
+        postUserMethod.setProperties("api/users/posts.properties");
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        postUserMethod.callAPI();
+        postUserMethod.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testCreateUserMissingSomeFields() throws Exception {
+        PostUserMethod postUserMethod = new PostUserMethod();
+        postUserMethod.setProperties("api/users/posts.properties");
+        postUserMethod.getProperties().remove("name");
+        postUserMethod.getProperties().remove("username");
+        postUserMethod.callAPIExpectSuccess();
+        postUserMethod.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testGetUsers() {
+        GetUserMethods getUsersMethods = new GetUserMethods();
+        getUsersMethods.callAPIExpectSuccess();
+        getUsersMethods.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
+        getUsersMethods.validateResponseAgainstSchema("api/users/_get/rs.schema");
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testDeleteUsers() {
+        DeleteUserMethod deleteUserMethod = new DeleteUserMethod(1);
+        deleteUserMethod.setProperties("api/users/posts.properties");
+        deleteUserMethod.callAPIExpectSuccess();
+        deleteUserMethod.validateResponse();
+    }*/
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testGetPosts() {
+        GetPostMethod getPostMethod = new GetPostMethod(3);
+        getPostMethod.setProperties("api/posts/posts.properties");
+        getPostMethod.callAPI();
+        getPostMethod.validateResponse();
+    }
+
+   @Test()
+    @MethodOwner(owner = "qpsdemo")
+   @TestPriority(Priority.P1)
+    public void testGetAllPosts() {
+        GetAllPostsMethod getAllPostsMethod = new GetAllPostsMethod();
+        getAllPostsMethod.callAPIExpectSuccess();
+        getAllPostsMethod.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testDeletePosts() {
+        DeletePostMethod deletePostMethod = new DeletePostMethod(4);
+        deletePostMethod.callAPI();
+        deletePostMethod.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testCreatePost() throws Exception {
+        PostPostMethod postPostMethod = new PostPostMethod();
+        postPostMethod.setProperties("api/posts/posts.properties");
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        postPostMethod.callAPI();
+        postPostMethod.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testUpdatePost() throws Exception {
+        PutPostMethod putPostMethod = new PutPostMethod(1);
+        putPostMethod.setProperties("api/posts/posts.properties");
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        putPostMethod.callAPI();
+        putPostMethod.validateResponse();
+    }
+
+}
